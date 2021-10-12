03161dd58e818a421a7fe02facbbf12a2331d99a
Tyler Say <sayqi@live.com>
2021-10-12 11:17:48 +0800

    deployment dotenv

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Movie from "./components/Movie";
 import NotFound from "./components/NotFound";
-require('dotenv').config()
+
 
 const App = () => (
   <Router>
